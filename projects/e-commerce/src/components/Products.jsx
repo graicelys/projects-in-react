6c36009314be049b1eb8612e5595c18c899fd270
“graicelys” <graicelys@gmail.com>
2024-07-10 17:58:37 -0400

    todo

---
+++ projects/e-commerce/src/components/Products.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import './Products.css';
+import { AddToCartIcon } from './Icons';
+
+export function Products({ products }) {
+    return (
+        <main className='products'>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img
+                            src={product.thumbnail}
+                            alt={product.title}
+                        />
+                        <div>
+                            <strong>{product.title}</strong>
+                        </div>
+                        <div>
+                        ${product.price}
+                        </div>
+                        <div>
+                            <button>
+                                <AddToCartIcon />
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
+}
+
+Products.propTypes = {
+    products: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        thumbnail: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+    })).isRequired,
+};
